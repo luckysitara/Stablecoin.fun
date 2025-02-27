8f09e4e87841023c4146f5442bf6d9513c268947
luckysitara <udoessienprecius@gmail.com>
2025-02-27 12:04:50 +0100

    refactoring the codebase

--- hooks/use-anchor-program.ts
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { useContext } from "react"
-import { AnchorContext } from "@/components/providers/anchor-provider"
-
-export function useAnchorProgram() {
-  return useContext(AnchorContext)
-}
-
