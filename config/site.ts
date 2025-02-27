d8665127b566456fb42032ef8b5f0029c26b22aa
luckysitara <udoessienprecius@gmail.com>
2025-02-27 22:09:53 +0100

    making fullstack

--- config/site.ts
+++
@@ -1,8 +0,0 @@
-export const siteConfig = {
-  name: "Stablecoin.fun",
-  description: "Create and manage stablecoins backed by Stablebond tokens.",
-  links: {
-    github: "https://github.com/luckysitara/Stablecoin.fun",
-  },
-}
-
